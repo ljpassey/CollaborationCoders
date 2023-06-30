d132a3ebe4335357dd2c03fb3809941249262684
Luke Passey <ljpassey@gmail.com>
2023-06-30 17:25:53 -0600

    Removed the tailwind config file, updated the favicon, removed the current global styles to prepare for pico, and updated the Nav compnent

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,19 +1,19 @@
-import './globals.css'
+import "./globals.css";
 
-import Nav from '../components/Nav'
+import Nav from "../components/Nav";
 
 export const metadata = {
-  title: 'Collaboration Coders',
-  description: 'Collection of blog posts from the Collaboration Coders community',
-}
+  title: "Collaboration Coders",
+  description:
+    "Collection of blog posts from the Collaboration Coders community",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="css/pico.min.css" />
+      </head>
       <body>
         <main>
           <Nav />
@@ -21,5 +21,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
--- blog/components/Nav.tsx
+++ blog/components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <nav>
         <Link href="/">
-            <Image src="/assets/icons/Logo.png" width={30} height={30} alt="collaboration coders logo" />
+            <Image src="/assets/images/logo.svg" width={30} height={30} alt="collaboration coders logo" />
         </Link>
         <div>
             <Link href="/about">
