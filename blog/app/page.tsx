d3ecea300c50c2d29929bacf10ec7717a363786e
Luke Passey <ljpassey@gmail.com>
2023-07-01 17:30:46 -0600

    Updated page.tsx to include more information and a rough outline of the page.

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import BlogCard from "@/components/BlogCard";
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <section>
+      <strong>
+        <h1>Welcome to Collaboration Coders!</h1>
+      </strong>
+      <article>
+        <h3>What is Collaboration Coders?</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, optio
+          cupiditate quod beatae ratione dolore dignissimos culpa id soluta
+          totam. Eaque facere deserunt excepturi numquam at provident incidunt
+          velit animi.
+        </p>
+        <nav>
+          <Link href="/about">Learn more about us</Link>
+        </nav>
+      </article>
+      <article>
+        <h3>View our most recent Blog post</h3>
+        <BlogCard />
+      </article>
+    </section>
+  );
 };
 
 export default Home;
