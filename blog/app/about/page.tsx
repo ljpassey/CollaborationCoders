d6dcb46bc13249fef187c3a1a6992a0027a206ae
Luke Passey <ljpassey@gmail.com>
2023-06-30 19:01:19 -0600

    Updated the about page

--- blog/app/about/page.tsx
+++ blog/app/about/page.tsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About Page</div>
+    <section>
+      <h1>About Collaboration Coders</h1>
+      <article>
+        <h3>Founded by Benjamin Thornhill and his trusty side-kick Luke Passey</h3>
+        <p>Collaboration Coders is a community of developers who are passionate about learning and sharing their knowledge with others.</p>
+
+      </article>
+      <article>
+        <h3>What we do</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, optio cupiditate quod beatae ratione dolore dignissimos culpa id soluta totam. Eaque facere deserunt excepturi numquam at provident incidunt velit animi.</p>
+      </article>
+      <article>
+        <h3>Who We Are</h3>
+        
+        <h4>Benjamin Thornhill</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, optio cupiditate quod beatae ratione dolore dignissimos culpa id soluta totam. Eaque facere deserunt excepturi numquam at provident incidunt velit animi.</p>
+        <h4>Luke Passey</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, optio cupiditate quod beatae ratione dolore dignissimos culpa id soluta totam. Eaque facere deserunt excepturi numquam at provident incidunt velit animi.</p>
+
+      </article>
+    </section>
   )
 }
 
