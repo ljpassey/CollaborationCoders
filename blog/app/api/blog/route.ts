1225dc41786112fd912b47b4e468b33524dc44fd
Ben Thornhill <25141193+bthornhill123@users.noreply.github.com>
2023-07-19 17:04:41 -0600

    Actually return users from blog endpoint
    Co-authored-by: Luke Passey <ljpassey@users.noreply.github.com>

--- blog/app/api/blog/route.ts
+++ blog/app/api/blog/route.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export const GET = async () => {
+export const GET = async (request: any, response: any) => {
     const users = await prisma.user.findMany()
     console.log('users :>> ', users);
+
+    // Return response code
+    return new Response(JSON.stringify(users), { status: 200 })
 }
