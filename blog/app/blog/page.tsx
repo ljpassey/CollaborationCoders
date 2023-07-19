6c367b74a6623e558b9e64f320724216d873b700
Luke Passey <ljpassey@gmail.com>
2023-07-19 17:47:55 -0600

    Updated the page.tsx file and got our server-side component pulling data from our sqlite db
    Co-authored-by: Ben Thornhill <bthornhill123@users.noreply.github.com>

--- blog/app/blog/page.tsx
+++ blog/app/blog/page.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-
 import BlogCard from "@/components/BlogCard";
+import { PrismaClient } from "@prisma/client";
 
-const Blog = () => {
+const prisma = new PrismaClient()
+
+async function getData() {
+  const users = await prisma.user.findMany()
+  const posts = await prisma.post.findMany()
+
+  return JSON.stringify({ users, posts })
+}
+
+// TODO - get strongly typed users
+export default async function Page() {
+  const data = await getData();
+  const {users, posts} = JSON.parse(data)
+  const userOne = users[0]
+
   return (
     <section>
       <h1>Blog</h1>
-      <BlogCard />
+      <h2>{userOne.name}</h2>
     </section>
   );
-};
-
-export default Blog;
+}
