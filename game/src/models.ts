977da5175936cea86c39f6805d84f633922f415e
Ben Thornhill <25141193+bthornhill123@users.noreply.github.com>
2023-10-15 13:44:08 -0600

    Finish stubbing out logic of stages for Game class

--- game/src/models.ts
+++ game/src/models.ts
@@ -12,60 +12,112 @@ export class Game {
 
   // g.selectPiece()
 
-  selectPiece(p: Piece): boolean {
-    // Set up state for the stage
+  selectPiece(piece: Piece): boolean {
+    // Stage setup
     this.selectedModifier = null;
     this.selectedDestination = null;
     this.stage = TurnStage.SELECT_PIECE;
 
     // Legality checks
-    if (this.stage != TurnStage.SELECT_PIECE) {
+    const isLegal = this.isPieceLegalToSelect(piece);
+    if (!isLegal) {
       return false;
     }
 
     // State changes
-    this.selectedPiece = p;
+    this.selectedPiece = piece;
     this.stage++;
+
     return true;
   }
 
-  selectModifier(modifierToSelect: Modifier): boolean {
-    // Set up state for the stage
+  isPieceLegalToSelect(piece: Piece | null): boolean {
+    if (!piece) {
+      return false;
+    }
+
+    const isCurrentPlayersPiece = piece.player == this.currentPlayer.name;
+    return isCurrentPlayersPiece;
+  }
+
+  selectModifier(modifier: Modifier): boolean {
+    // Stage setup
     this.selectedDestination = null;
     this.stage = TurnStage.SELECT_MODIFIER;
 
-    const playersModifier = this.currentPlayer.modifiers.find(
-      (m) => m.type == modifierToSelect.type
+    const isLegal = this.isModifierLegalToSelect(modifier);
+    if (!isLegal) {
+      return false;
+    }
+
+    // State changes
+    this.selectedModifier = modifier;
+    this.stage++;
+
+    return true;
+  }
+
+  isModifierLegalToSelect(modifier: Modifier | null): boolean {
+    if (!modifier) {
+      return false;
+    }
+
+    const playerModifier = this.currentPlayer.modifiers.find(
+      (m) => m.type == modifier.type
     );
-    if (playersModifier?.count == 0) {
+    if (!playerModifier || playerModifier.count == 0) {
       return false;
     }
-
-    // Legality checks
-    if (this.stage != TurnStage.SELECT_MODIFIER) {
-      return false;
-    }
-
-    this.selectedModifier = modifierToSelect;
 
     return true;
   }
 
   selectDestination(destination: Piece): boolean {
+    // Stage setup
+    this.stage = TurnStage.SELECT_DESTINATION;
+
     // Legality checks
-    if (this.stage != TurnStage.SELECT_DESTINATION) {
+    const isLegal = this.isDestinationLegalToSelect(destination);
+    if (!isLegal) {
       return false;
     }
 
+    // State changes
     this.selectedDestination = destination;
+    this.stage++;
+
     return true;
   }
 
-  endTurn() {
-    // Empty out the place where a piece was moved from
-    // Update the destination where a piece was moved to
-    // Decrement player modifiers that were used
-    // Flipping the current player owndership to the opponent
+  isDestinationLegalToSelect(destination: Piece | null): boolean {
+    if (!destination) {
+      return false;
+    }
+
+    const isCurrentPlayersPiece = destination.player == this.currentPlayer.name;
+    const isInRange = true; // TODO - uses this.selectedModifier
+
+    return !isCurrentPlayersPiece && isInRange;;
+  }
+
+  endTurn(): boolean {
+    // Verify legality of turn (one last time)
+    if (!this.isPieceLegalToSelect(this.selectedPiece)) {
+      return false;
+    }
+    if (!this.isModifierLegalToSelect(this.selectedModifier)) {
+      return false;
+    }
+    if (!this.isDestinationLegalToSelect(this.selectedDestination)) {
+      return false;
+    }
+
+    // TODO - Empty out the place where a piece was moved from
+    // TODO - Update the destination where a piece was moved to
+    // TODO - Decrement player modifiers that were used
+    // TODO - Flipping the current player owndership to the opponent
+
+    return true;
   }
 }
 
@@ -74,7 +126,7 @@ export class Piece {
     public player: 'X' | 'O' | '',
     public position: Position,
     public isSelected: boolean = false
-  ) {}
+  ) { }
 }
 
 export class Player {
@@ -82,11 +134,11 @@ export class Player {
   modifiers: Modifier[] = [];
 }
 export class Position {
-  constructor(public row: number, public col: number) {}
+  constructor(public row: number, public col: number) { }
 }
 
 export class Modifier {
-  constructor(public type: 'Pawn' | 'Rook' | 'Queen', public count: number) {}
+  constructor(public type: 'Pawn' | 'Rook' | 'Queen', public count: number) { }
 }
 
 export enum TurnStage {
