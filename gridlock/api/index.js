e0e4bab136881c2e5e95beb3ce4f3e72da20e54d
Luke Passey <ljpassey@gmail.com>
2024-03-17 11:29:18 -0600

    add cors

--- gridlock/api/index.js
+++ gridlock/api/index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const { SERVER_PORT } = process.env;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
